5b2c1daf1dc7399f18b8133e3e9c7211c7692def
Antonio Caristia <a.caristia@gmail.com>
2019-02-24 20:19:18 +0100

    main cleaned out

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from flask import Flask, request
 from flask_restful import Api
 
-import pdb
 from api.resource import KeepAlive
 from api.resource import Consumer
 from api.resource import ConsumerList
 from api.resource import CoupleList
-
-#from adapter.concrete import AdapterDocker
 
 app = Flask(__name__)
 api = Api(app)
@@ -16,28 +13,6 @@ api.add_resource(KeepAlive, '/keepalive')
 api.add_resource(Consumer, '/consumer/<string:c_id>')
 api.add_resource(ConsumerList, '/consumers')
 api.add_resource(CoupleList, '/couples')
-
-#"""
-#curl -H "Content-Type: application/json" POST -d \
-#'{"id":1003, "action":"like", "user":"a.caristia@gmail.com", "type":"news"}' \
-#http://localhost:5002/
-#"""
-#
-#@app.route("/", methods=['POST'])
-#def hello():
-#    #data = json.loads(request.data.decode('utf-8'))
-#    data = json.loads(request.data.decode('utf-8'))
-#    print(data)
-#    return "Hello World!"
-#
-#@app.route("/delete_me", methods=['POST'])
-#def delete_me():
-#    data = json.loads(request.data.decode('utf-8'))
-#    print(data)
-#
-#    runtime_soucre = AdaperDocker(data).create()
-#    return "Hello World!"
-#
 
 if __name__ == '__main__':
 
