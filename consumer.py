4f008a6d38954e8ad6c31f91721addb6ed851a54
Antonio Caristia <a.caristia@gmail.com>
2019-02-24 20:13:47 +0100

    consumer cleaned out

--- consumer.py
+++ consumer.py
@@ -6,38 +6,12 @@ from settings import host_rabbit, vhost, queue, exchange, exchange_type, routing
 # docker-compose doesn't garantee anything about services startup order
 # so wait until rabbirmq connection is available
 
-#import pika
-#import time
-#
-#while(True):
-#    try:
-#
-#        if conn_details['vhost'] is None:
-#            connection = pika.BlockingConnection(pika.ConnectionParameters(conn_details['host']))
-#        else:
-#            connection = pika.BlockingConnection(pika.ConnectionParameters(host=conn_details['host'],virtual_host=conn_details['vhost']))
-#        
-#        if connection.is_open:
-#            print('OK, rabbitmq is ready. Closing this connection and starting consumer!')
-#            connection.close()
-#            break
-#        else:
-#            print("wait until rabbimq is up and running...")
-#            time.sleep(5)
-#
-#    except Exception as error:
-#        print("wait until rabbimq is up and running...")
-#        time.sleep(5)
-#
-
 result = Consumer.register('docker','ubuntu', wait_for_teaster)
 
 print("registration result \n{}".format(json.dumps(result, indent=4, sort_keys=True)))
 
 queue = result['id']
 routing = queue
-
-#queue = 'test'
 
 #print("queue: {}".format(queue))
 
