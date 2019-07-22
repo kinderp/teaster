da1a83d09dde86a7e4ebda2c7d36a6694036e76b
antonio <a.caristia@gmail.com>
2019-07-22 09:07:56 +0200

    Add Dockerfile for icelery and rabbit_celery
    - Dockerfile_icelery
    - Dockerfile_rabbit_celery
    
    - init_rabbitmq_celery.sh
      this file contains init confs for rabbit_celery.

---
+++ init_rabbitmq_celery.sh
@@ -0,0 +1,13 @@
+# Create Rabbitmq user
+( sleep 15 ; \
+rabbitmqctl add_user celery celery ; \
+rabbitmqctl add_vhost celery ; \
+rabbitmqctl set_user_tags celery celery ; \
+rabbitmqctl set_permissions -p celery celery ".*" ".*" ".*" ; \
+echo "*** User celery completed. ***" ; \
+echo "*** Log in the WebUI at port 15673 (example: http:/localhost:15673) ***") &
+
+# $@ is used to pass arguments to the rabbitmq-server command.
+# For example if you use it like this: docker run -d rabbitmq arg1 arg2,
+# it will be as you run in the container rabbitmq-server arg1 arg2
+rabbitmq-server $@
